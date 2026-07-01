4b1e212e66d30ae494676894b99eaa4e18bc55f3
unknown <sc23s2e@leeds.ac.uk>
2026-07-01 16:02:17 +0100

    Basic WandB pipeline with optuna

--- src/wandb_pipeline.py
+++ src/wandb_pipeline.py
@@ -9,12 +9,29 @@ from xgboost import XGBClassifier
 from sklearn.neural_network import MLPClassifier
 
 from data_experiments import import_data
-from preprocessing_pipeline import prepare_data, CustomImputer
 import time
 
 from sklearn.metrics import accuracy_score, log_loss, f1_score
 
 import pandas as pd
+
+import wandb 
+import optuna
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class CustomImputer(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+
+        #need to drop duration (cant know before call) - keep it in
+        X = X.drop('duration', axis=1, errors='ignore')
+
+        return X
 
 #preprocesses the data as it would in the pipeline
 def load_and_preprocess():
@@ -25,15 +42,20 @@ def load_and_preprocess():
     Y_prepared = le.fit_transform(Y["y"])
     Y = pd.Series(Y_prepared)
     
-    #split data
+    #split data into train and test
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, test_size=0.2, random_state=42
+    )
+
+    #then split train 70/30 into train/validate (not sure if this is the righ way)
+    X_train, X_validate, Y_train, Y_validate = train_test_split(
+        X_train, Y_train, test_size=0.2, random_state=42
     )
 
     print("Imputing Data")
     imputer = CustomImputer()
     X_train = imputer.fit_transform(X_train)
-    X_test = imputer.transform(X_test)
+    X_validate = imputer.transform(X_validate)
 
     print("Processing data")
     num_cols = X_train.select_dtypes(include='number').columns
@@ -45,7 +67,7 @@ def load_and_preprocess():
 
     #do the encoding here (only transform the test)
     X_train_enc = encoder_scalar.fit_transform(X_train)
-    X_test_final = encoder_scalar.transform(X_test)
+    X_validate_final = encoder_scalar.transform(X_validate)
 
     #SMOTE training data
     custom_smote = SMOTE(k_neighbors=7, random_state=42)
@@ -55,7 +77,7 @@ def load_and_preprocess():
     X_train_final, Y_train_bal = smote_enn.fit_resample(X_train_enc, Y_train)
     print(f"Preprocessing complete. Balanced Training Shape: {X_train_final.shape}")
     
-    return X_train_final, X_test_final, Y_train_bal, Y_test
+    return X_train_final, X_validate_final, Y_train_bal, Y_validate
 
 #pass in the training data and the parameters for the model
 #return the created model
@@ -64,11 +86,47 @@ def create_xgb_model(X_train, Y_train, params):
     model.fit(X_train, Y_train)
     return model
 
-if __name__ == "__main__":
-    import wandb 
+#using an optuna trial (start with just 2 variables to log)
+def param_trial(trial, X_train, Y_train, X_validate, Y_validate, params, run):
+    #adjust params
+    params['n_estimators'] = trial.suggest_int('n_estimators', 50, 750)
+    params['learning_rate'] = trial.suggest_float('learning_rate', 0, 2)
 
+    #create model
+    xgb_model = create_xgb_model(X_train, Y_train, params)
+
+    #predict and log values
+    #inference time
+    start_time = time.perf_counter()
+    preds = xgb_model.predict(X_validate)
+    inference_time = time.perf_counter() - start_time
+
+    #probability for loss calculatoin
+    probs = xgb_model.predict_proba(X_validate)
+    
+    #accuracy metrics
+    f1 = f1_score(Y_validate, preds)
+    acc = accuracy_score(Y_validate, preds)
+    loss = log_loss(Y_validate, probs)
+
+    #logging values by trial
+    run.log({
+        "Accuracy": acc, 
+        "Loss": loss,
+        "F1 Score": f1,
+        "Time": inference_time,
+        "Learning Rate": params['learning_rate'],
+        "Num estimators": params['n_estimators'],
+        "Trial": trial.number,
+        "Depth": 7,
+    })
+
+    #goal is to maximise f1 & minimise time
+    return loss
+
+if __name__ == "__main__":
     #preprocess data
-    X_train, X_test, Y_train, Y_test = load_and_preprocess()
+    X_train, X_validate, Y_train, Y_validate = load_and_preprocess()
 
     #setup a wandb run
     run = wandb.init(
@@ -77,54 +135,37 @@ if __name__ == "__main__":
         config={
             "model": "XGBoost",
             "dataset": "Portugese Telemarketing",
-            "n_estimators": 275,
-            "learning_rate": 0.03,
+            "max_depth": 7,
             "subsample": 0.8,
             "colsample_bytree": 0.7,
             "eval_metric": "logloss",
         },
     )
 
-    #define depth as custom x axis - want everything else to step with it
+    #block out for now - only want trial
+    run.define_metric("Trial")
+    run.define_metric("Accuracy", step_metric="Trial")
+    run.define_metric("Loss", step_metric="Trial")
+    run.define_metric("F1 Score", step_metric="Trial")
+    run.define_metric("Time", step_metric="Trial")
+    run.define_metric("Learning Rate", step_metric="Trial")
+    run.define_metric("Num estimators", step_metric="Trial")
     run.define_metric("Depth")
-    run.define_metric("Accuracy", step_metric="Depth")
-    run.define_metric("Loss", step_metric="Depth")
-    run.define_metric("F1 Score", step_metric="Depth")
-    run.define_metric("Inference Time", step_metric="Depth")
 
-    #want to experiment with different depths
-    for depth in [4, 5, 6, 7, 8]:
+    #creating the optuna trial
+    print("XGBoost Optimisation")
+    
+    #initial params
+    params = {
+        "max_depth": 7,
+        "n_estimators": 275,
+        "learning_rate": 0.03,
+        "subsample": 0.8,
+        "colsample_bytree": 0.7,
+        "eval_metric": "logloss",
+    }
 
-        params = {
-            "n_estimators": 275,
-            "learning_rate": 0.03,
-            "subsample": 0.8,
-            "max_depth": depth,
-            "colsample_bytree": 0.7,
-            "eval_metric": "logloss",
-        }
-
-        xgb_model = create_xgb_model(X_train, Y_train, params)
-
-        #inference time
-        start_time = time.perf_counter()
-        preds = xgb_model.predict(X_test)
-        inference_time = time.perf_counter() - start_time
-
-        #probability for loss calculatoin
-        probs = xgb_model.predict_proba(X_test)
-
-        f1 = f1_score(Y_test, preds)
-        
-        # 3. Calculate metrics
-        acc = accuracy_score(Y_test, preds)
-        loss = log_loss(Y_test, probs)
-
-        run.log({"Accuracy": acc, 
-            "F1 Score": f1, 
-            "Loss": loss,
-            "Inference Time": inference_time,
-            "Depth": depth 
-        })
+    study_xgb = optuna.create_study(directions=['minimize'], study_name="XGB_Time_vs_F1")
+    study_xgb.optimize(lambda trial: param_trial(trial, X_train, Y_train, X_validate, Y_validate, params, run), n_trials=30)
 
     run.finish()
